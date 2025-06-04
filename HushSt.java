6235b23c544ddad4e3340986462a33c709e08897
sankalp789 <vermasankalp08@gmail.com>
2025-06-04 11:11:29 +0530

    Add files via upload

---
+++ HushSt.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class HushSt {
+    public static void main(String[] args) {
+        HashSet<String> set= new HashSet<>();
+        set.add("C");
+        set.add("B");
+        set.add("A");
+        set.add("a");
+        System.out.println(set);
+
+    }
+}
