6400966a4986190fc6784abfa4005569d9787254
sankalp789 <vermasankalp08@gmail.com>
2025-06-04 11:10:50 +0530

    Add files via upload

---
+++ TreSt.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class TreSt {
+    public static void main(String[] args) {
+        TreeSet<String> set=new TreeSet<>();
+        set.add("C");
+        set.add("B");
+        set.add("A");
+        set.add("a");
+        System.out.println(set);
+    }
+
+}
